f2668ffcb6b29abfdb007b7443ffba2aa2da45bf
AsterCasc <astercass@qq.com>
2026-06-10 15:13:03 +0800

    feat: support navigation

--- src/ui/views/ZyySubsystemMain.vue
+++ src/ui/views/ZyySubsystemMain.vue
@@ -16,7 +16,7 @@
                          :enable="video.enable"
                          :base-size="16"
                          :disable-text="$t('in_develop_simple')"
-                         :on-click="()=> {}"/>
+                         :on-click="()=> { jumpToSubsystem(video.enable, video.navigationName) }"/>
       </div>
 
     </q-scroll-area>
@@ -29,7 +29,7 @@ import CaskFloatCard from "@/ui/components/CaskFloatCard.vue";
 import {ref} from "vue";
 import {useGlobalStateStore} from "@/utils/global-state.js";
 import {useRouter} from "vue-router";
-
+import {toSpecifyPage} from "@/router/index.js";
 
 const globalState = useGlobalStateStore();
 const thisRouter = useRouter()
@@ -39,46 +39,60 @@ const systemList = ref([
     collectionImg: "/img/subsystem/appointment.jpg",
     collectionImgExtra: "/img/subsystem/appointment.webp",
     collectionName: "zyy_subsystem_appointment",
+    navigationName: "book",
     enable: true,
   },
   {
     collectionImg: "/img/subsystem/staff.jpg",
     collectionImgExtra: "/img/subsystem/staff.webp",
     collectionName: "zyy_subsystem_staff",
+    navigationName: "staff",
     enable: true,
   },
   {
     collectionImg: "/img/subsystem/agent.jpg",
     collectionImgExtra: "",
     collectionName: "zyy_subsystem_agent",
+    navigationName: "agent",
     enable: false,
   },
   {
     collectionImg: "/img/subsystem/bill.jpg",
     collectionImgExtra: "",
     collectionName: "zyy_subsystem_bill",
+    navigationName: "bill",
     enable: false,
   },
   {
     collectionImg: "/img/subsystem/order.jpg",
     collectionImgExtra: "",
     collectionName: "zyy_subsystem_order",
+    navigationName: "order",
     enable: false,
   },
   {
     collectionImg: "/img/subsystem/page.jpg",
     collectionImgExtra: "",
     collectionName: "zyy_subsystem_page",
+    navigationName: "page",
     enable: false,
   },
   {
     collectionImg: "/img/subsystem/user.jpg",
     collectionImgExtra: "/img/subsystem/user.webp",
     collectionName: "zyy_subsystem_user",
+    navigationName: "user",
     enable: true,
   },
-
 ])
+
+
+function jumpToSubsystem(enable, navigationName) {
+  if (!enable) {
+    return
+  }
+  toSpecifyPage(thisRouter, navigationName)
+}
 
 </script>
 
